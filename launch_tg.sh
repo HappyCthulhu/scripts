d12d15b40d8aa5146a949bcab40917dfee44c6eb
Valeriy <valeriyg98@gmail.com>
2024-06-04 17:42:16 +0200

    Add code in launch_tg.sh

--- launch_tg.sh
+++ launch_tg.sh
@@ -1,1 +1,9 @@
-launch_tg.sh
+window_exists=$(swaymsg -t get_tree | jq '[recurse(.nodes[]?, .floating_nodes[]?) | select(.name == "Telegram Web")] | length > 0')
+
+if [ "$window_exists" = "false" ]; then
+    google-chrome-stable --app=https://web.telegram.org/k/
+fi
+
+swaymsg [con_mark="tg"] scratchpad show
+swaymsg [con_mark="tg"] resize set width 50ppt height 80ppt
+swaymsg [con_mark="tg"] move position center center
